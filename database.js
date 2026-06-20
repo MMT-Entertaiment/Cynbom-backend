e0ff23d6262a9ad45ea13cf6380587f19ee7af45
MMT <uzyd00rm4n@gmail.com>
2026-06-20 13:06:44 +0200

    Add url_id parameter to series insertion

--- database.js
+++ database.js
@@ -44,7 +44,7 @@ module.exports = {
       for (let i = 0; i < 35; i++) url_id += chars[Math.floor(Math.random() * chars.length)];
     }
     const stmt = db.prepare('INSERT INTO series (titre, studio, annee, age, genre, image, url_id) VALUES (?, ?, ?, ?, ?, ?, ?)');
-    return stmt.run(titre, studio, annee, age, genre, image || null);
+    return stmt.run(titre, studio, annee, age, genre, image || null, url_id);
   },
 
   supprimerSerie(titre) {
